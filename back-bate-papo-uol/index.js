3dd1233139598e837e9817f1e18de2c0db4a0077
nando-castro <fernando.castro10.master@gmail.com>
2022-06-23 17:05:14 -0300

    post participants

---
+++ back-bate-papo-uol/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cors from 'cors';
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+
+mongoClient.connect(() => {
+    db = mongoClient.db('batepapo_uol');
+});
+
+const app = express();
+app.use(express.json());
+
+/* Participants Routes */
+
+app.post('/participants', async (req,res) => {
+    
+    const participant = req.body;
+
+    try {
+        await db.collection('participants').insertOne(participant);
+        res.sendStatus(201);
+    } catch (error) {
+        res.sendStatus(422);
+    }
+});
+
+app.listen(5000, () => {
+    console.log('Server is litening on port 5000.');
+    console.log('http://localhost:5000');
+})
