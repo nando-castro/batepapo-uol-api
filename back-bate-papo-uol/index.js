b932f7285f6585791dabe610bca756f22ccb0cfe
nando-castro <fernando.castro10.master@gmail.com>
2022-06-23 17:09:55 -0300

    get participants

--- back-bate-papo-uol/index.js
+++ back-bate-papo-uol/index.js
@@ -1,14 +1,14 @@
-import express from 'express';
-import cors from 'cors';
-import { MongoClient } from 'mongodb';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 dotenv.config();
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
 mongoClient.connect(() => {
-    db = mongoClient.db('batepapo_uol');
+  db = mongoClient.db("batepapo_uol");
 });
 
 const app = express();
@@ -16,19 +16,32 @@ app.use(express.json());
 
 /* Participants Routes */
 
-app.post('/participants', async (req,res) => {
-    
-    const participant = req.body;
+//GET
 
-    try {
-        await db.collection('participants').insertOne(participant);
-        res.sendStatus(201);
-    } catch (error) {
-        res.sendStatus(422);
-    }
+app.get("/participants", async (req, res) => {
+  try {
+    const participants = await db.collection('participants').find().toArray();
+    res.send(participants);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+//POST
+
+app.post("/participants", async (req, res) => {
+  const participant = req.body;
+
+  try {
+    await db.collection("participants").insertOne(participant);
+    res.sendStatus(201);
+  } catch (error) {
+    res.sendStatus(422);
+  }
 });
 
 app.listen(5000, () => {
-    console.log('Server is litening on port 5000.');
-    console.log('http://localhost:5000');
-})
+  console.log("Server is litening on port 5000.");
+  console.log("http://localhost:5000");
+});
