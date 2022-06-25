ab4e2ddcba4f5e3ea3b2834f717f1ab302d65ef6
nando-castro <fernando.castro10.master@gmail.com>
2022-06-25 16:14:34 -0300

    validation joi

--- back-bate-papo-uol/index.js
+++ back-bate-papo-uol/index.js
@@ -3,7 +3,8 @@ import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import dayjs from "dayjs";
-import joi from 'joi';
+import joi from "joi";
+import e from "express";
 dotenv.config();
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
@@ -16,6 +17,16 @@ mongoClient.connect(() => {
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const participantsSchema = joi.object({
+  name: joi.string().min(1).required(),
+});
+
+const messageSchema = joi.object({
+  to: joi.string().min(1).required(),
+  text: joi.string().min(1).required(),
+  type: joi.string().valid("message", "private_message").required(),
+});
 
 /* Participants Routes */
 
@@ -33,25 +44,36 @@ app.get("/participants", async (req, res) => {
 //POST
 app.post("/participants", async (req, res) => {
   const { name } = req.body;
+  const validacao = participantsSchema.validate({name: name});
+  const { error } = validacao;
 
-  if (name === null || name === "") {
-    res.sendStatus(422);
-  } else {
-    try {
-      await db
-        .collection("participants")
-        .insertOne({ name: name, lastStatus: Date.now() });
-      await db.collection("message").insertOne({
-        from: name,
-        to: "Todos",
-        text: "entra na sala...",
-        type: "status",
-        time: dayjs().format("HH:mm:ss"),
-      });
-      res.sendStatus(201);
-    } catch (error) {
-      res.sendStatus(500);
-    }
+  if (error) {
+    const details = error.details.map((e) => e.message);
+    res.status(422).send(details);
+    return;
+  }
+
+  const participant = await db.collection("participants").findOne({name: req.body});
+
+  if(participant){
+    res.sendStatus(409);
+    return;
+  }
+
+  try {
+    await db
+      .collection("participants")
+      .insertOne({ name: name, lastStatus: Date.now() });
+    await db.collection("message").insertOne({
+      from: name,
+      to: "Todos",
+      text: "entra na sala...",
+      type: "status",
+      time: dayjs().format("HH:mm:ss"),
+    });
+    res.sendStatus(201);
+  } catch (error) {
+    res.sendStatus(500);
   }
 });
 
@@ -60,18 +82,34 @@ app.post("/participants", async (req, res) => {
 //POST
 app.post("/messages", async (req, res) => {
   const { to, text, type } = req.body;
+  //const message = req.body;
+
   const { user } = req.headers;
+  
+  /* const validacao = messageSchema.validate(to, text, type, user);
+  
+  const { error } = validacao;
+  
+  if (error) {
+    const details = error.details.map((e) => e.message);
+    res.status(422).send(details);
+    return;
+  } */
+ /*  const participant = await db.collection("participants").findOne(user);
+
+  if(!participant){
+    res.status(422).send("No valid user!!");
+    return;
+  } */
 
   try {
-    await db
-      .collection("messages")
-      .insertOne({
-        from: user,
-        to: to,
-        text: text,
-        type: type,
-        time: dayjs().format("HH:mm:ss"),
-      });
+    await db.collection("messages").insertOne({
+      from: user,
+      to: to,
+      text: text,
+      type: type,
+      time: dayjs().format("HH:mm:ss"),
+    });
     res.sendStatus(201);
   } catch (error) {
     res.sendStatus(500);
@@ -89,6 +127,13 @@ app.get("/messages", async (req, res) => {
   }
 });
 
+/* Status Routes */
+
+//POST
+/* app.post("/status", async (req, res) => {
+  const user = req.headers.user;
+});
+ */
 app.listen(5000, () => {
   console.log("Server is litening on port 5000.");
   console.log("http://localhost:5000");
